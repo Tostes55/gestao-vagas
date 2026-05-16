dfedddde6941ab7d460095ea627a4b298543ebc5
Victor Tostes <114588256+Tostes55@users.noreply.github.com>
2026-05-16 09:34:28 -0300

    feat: Implementado validações na criacao do candidato

--- src/main/java/com/gerenciador/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/gerenciador/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,4 +1,28 @@
-package com.gerenciador.gestao_vagas.modules.candidate.controller;
+package com.gerenciador.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
+
+@Data
 public class CandidateEntity {
+    private UUID id;
+    private String name;
+
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
+    private String username;
+
+    @Email(message = "O campo [email] deve conter um email válido")
+    private String email;
+
+    @Length(min = 10, max = 100)
+    private String password;
+    private String description;
+    private String curriculum;
+
 }
