deaa44cee646c7d75db60a58d67232b05d3c4127
Victor Tostes <114588256+Tostes55@users.noreply.github.com>
2026-05-17 09:43:09 -0300

    Implementado try catch no cadastro do candidato

--- src/main/java/com/gerenciador/gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/com/gerenciador/gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -3,8 +3,10 @@ package com.gerenciador.gestao_vagas.modules.candidate.controller;
 import com.gerenciador.gestao_vagas.modules.candidate.exceptions.UserFoundException;
 import com.gerenciador.gestao_vagas.modules.candidate.model.CandidateEntity;
 import com.gerenciador.gestao_vagas.modules.candidate.repository.CandidateRepository;
+import com.gerenciador.gestao_vagas.modules.candidate.service.CandidateService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,18 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateController {
 
     @Autowired
-    private CandidateRepository candidateRepository;
+    private CandidateService candidateService;
 
     @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
 
-        this.candidateRepository
-                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
-                .ifPresent((user)-> {
-                    throw new UserFoundException();
-                });
+        try {
+            var result = this.candidateService.createCandidate(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
 
-
-        return this.candidateRepository.save(candidateEntity);
     }
 }
--- src/main/java/com/gerenciador/gestao_vagas/modules/candidate/service/CandidateService.java
+++ src/main/java/com/gerenciador/gestao_vagas/modules/candidate/service/CandidateService.java
@@ -1,4 +1,25 @@
 package com.gerenciador.gestao_vagas.modules.candidate.service;
 
+import com.gerenciador.gestao_vagas.modules.candidate.exceptions.UserFoundException;
+import com.gerenciador.gestao_vagas.modules.candidate.model.CandidateEntity;
+import com.gerenciador.gestao_vagas.modules.candidate.repository.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CandidateService {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public CandidateEntity createCandidate(CandidateEntity candidateEntity) {
+        this.candidateRepository
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user)-> {
+                    throw new UserFoundException();
+                });
+
+        return this.candidateRepository.save(candidateEntity);
+    }
+
 }
