ccfeb307025b4a4bba6c4a3a5a3e076296c14642
Victor Tostes <victor.o.tostes@gmail.com>
2026-06-03 19:34:54 -0300

    feat: Incluido service de aplicacao da vaga

---
+++ src/main/java/com/gerenciador/gestao_vagas/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.gerenciador.gestao_vagas.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+}
---
+++ src/test/java/com/gerenciador/gestao_vagas/PrimeiroTeste.java
@@ -0,0 +1,4 @@
+package com.gerenciador.gestao_vagas;
+
+public class PrimeiroTeste {
+}
